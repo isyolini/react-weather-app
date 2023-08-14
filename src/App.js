a750d3ccfa65487b2ebf077bc25248cd3d03ef16
isyolini <50636149+isyolini@users.noreply.github.com>
2023-08-14 19:46:32 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <footer>
+        <a
+          href="https://github.com/isyolini/react-weather-app.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://www.linkedin.com/in/isabellarossolini/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          IsyOlini
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
