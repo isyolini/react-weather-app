711d0caf79a608a9d9d404372aab500fb9eed34e
isyolini <50636149+isyolini@users.noreply.github.com>
2023-08-14 22:41:21 +0200

    Basic structure html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
 
         <footer>
           <a
