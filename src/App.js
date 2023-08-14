81d132bc4abe82b93380716d0f6f8719396d0b5a
isyolini <50636149+isyolini@users.noreply.github.com>
2023-08-14 20:05:22 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        <a
-          href="https://github.com/isyolini/react-weather-app.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>{" "}
-        by{" "}
-        <a
-          href="https://www.linkedin.com/in/isabellarossolini/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          IsyOlini
-        </a>
-      </footer>
+        <footer>
+          <a
+            href="https://github.com/isyolini/react-weather-app.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://www.linkedin.com/in/isabellarossolini/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            IsyOlini
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
