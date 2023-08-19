bc44b23b8ce1a841e19300fc19e5e00bd6b8fdf4
isyolini <50636149+isyolini@users.noreply.github.com>
2023-08-19 22:20:04 +0200

    Styled the forecast of 1 day with css

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,20 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
-  return "Weather Forecast!";
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <WeatherIcon code="clear-sky-day" className="Icon" size={32} />
+          <div className="WeatherForecast-temperature">
+            <span className="WeatherForecast-temperature-max">19º</span>
+            <span className="WeatherForecast-temperature-min">10º</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,12 +16,18 @@ export default function WeatherInfo(props) {
       </ul>
 
       <div className="row mt-3">
-        <div className="col-8">
-          <WeatherIcon code={props.data.icon} />
-          <WeatherTemperature celsius={props.data.temperature} />
+        <div className="col-6">
+          <div className="d-flex">
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} size={60} />
+            </div>
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
         </div>
 
-        <div className="col-4">
+        <div className="col-6">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {Math.round(props.data.wind)} km/h</li>
